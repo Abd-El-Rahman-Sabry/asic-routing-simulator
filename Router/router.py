58f14a195b539e02bf102127706c5e2b4b313feb
Abd-El-Rahman-Sabry <aersabry@gmail.com>
2024-11-27 08:10:23 -0800

    refactoring
    adding some refactoring

--- Router/router.py
+++ Router/router.py
@@ -1,39 +1,10 @@
-import pygame 
-import colors
+class Router:
 
+    def __init__(self, grid):
+        self.__grid = grid
 
-SCREEN_WIDTH = 600 
-SCREEN_HEIGHT = 600
-TITLE = "Router"
-GRID_COLOR = (126,126,126)
+    def route(self, start, end):
+        return NotImplementedError
 
-WIN = pygame.display.set_mode([SCREEN_WIDTH , SCREEN_HEIGHT])
-
-
-
-class RouterMainWindow:
-
-
-    def __init__(self , width : float = SCREEN_WIDTH , height : float = SCREEN_HEIGHT) -> None:
-        self.__width = width 
-        self.__height = height 
-    
-        
-        pygame.display.set_caption(TITLE)
-    
-
-    def draw_grid(self, rows , width , **kwarg):
-        
-        gap = width // rows 
-
-        color = kwarg.get("color" , GRID_COLOR)
-
-        for i in range(rows): 
-            pygame.draw.line(WIN , color, (i * gap  , 0 ) , (i * gap , width) , )
-
-        for i in range(rows):  
-            pygame.draw.line(WIN , color, (0 , i * gap ) , (width , i * gap))
-
-
-
-
+    def run(self, start, ends: list[object]):
+        return NotImplementedError
